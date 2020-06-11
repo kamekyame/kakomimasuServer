97874087eaa06f77f1f1325f49cad9c1d426a5c4
Suzu Tomo <47011206+kamekyame@users.noreply.github.com>
2020-06-11 13:55:47 +0900

    Add: vscode用の設定ファイルの追加

--- main.ts
+++ main.ts
@@ -7,6 +7,3 @@ import { routes } from "./router.ts";
 const app = createApp();
 app.route("/", routes());
 app.listen({ port: 8888 });
-
-//実行コマンド
-// curl --request POST --url http://localhost:8888/match --header "Content-Type: application/json" --data "{\"userName\":\"SuzuTomo\",\"spec\":\"people\"}" -v
